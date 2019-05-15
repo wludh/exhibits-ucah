8b2e52c657e75b0bd66752677a65352b3a5fac1a
mackenziekbrooks <mackenziekbrooks@gmail.com>
2019-05-15 11:50:52 -0400

    added prev next buttons to bottom of item metadata; increased font

--- items/show.php
+++ items/show.php
@@ -78,6 +78,12 @@ echo head(array('title' => $title, 'bodyclass' => 'items show' .  (($hasImages) 
         <h3><?php echo __('Citation'); ?></h3>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
+     <nav>
+    <ul class="item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+    </ul>
+    </nav>
     
     
     <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
